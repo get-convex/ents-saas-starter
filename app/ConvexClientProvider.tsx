c7f05cf1d090ab64e88a65a77407fe8440b0bb77
Michal Srb <srb@convex.dev>
2024-06-03 10:35:28 +0200

    Move ClerProvider to root

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -1,31 +1,21 @@
 "use client";
 
 import { api } from "@/convex/_generated/api";
-import { ClerkProvider, useAuth, useUser } from "@clerk/nextjs";
+import { useAuth, useUser } from "@clerk/nextjs";
 import { Authenticated, ConvexReactClient, useMutation } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 import { ReactNode, useEffect } from "react";
-import { useMediaQuery } from "usehooks-ts";
-import { ErrorBoundary } from "./ErrorBoundary";
-import { dark } from "@clerk/themes";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   return (
-    <ErrorBoundary>
-      <ClerkProvider
-        appearance={{ baseTheme: prefersDarkMode ? dark : undefined }}
-      >
-        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-          <Authenticated>
-            <StoreUserInDatabase />
-          </Authenticated>
-          {children}
-        </ConvexProviderWithClerk>
-      </ClerkProvider>
-    </ErrorBoundary>
+    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+      <Authenticated>
+        <StoreUserInDatabase />
+      </Authenticated>
+      {children}
+    </ConvexProviderWithClerk>
   );
 }
 
--- app/DashboardButtons.tsx
+++ app/DashboardButtons.tsx
@@ -3,35 +3,28 @@
 import { ErrorBoundary } from "@/app/ErrorBoundary";
 import { Button } from "@/components/ui/button";
 import { SignInButton, SignUpButton } from "@clerk/nextjs";
-import {
-  ClerkLoading,
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-} from "@clerk/nextjs";
+import { ClerkLoading, SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 
 export function DashboardButtons() {
   return (
     <ErrorBoundary>
-      <ClerkProvider>
-        <ClerkLoading>
-          <div className="w-40 h-9" />
-        </ClerkLoading>
-        <SignedIn>
-          <OpenDashboardLinkButton />
-        </SignedIn>
-        <SignedOut>
-          <div className="flex gap-4 animate-[fade-in_0.2s]">
-            <SignInButton mode="modal" forceRedirectUrl="/t">
-              <Button variant="ghost">Sign in</Button>
-            </SignInButton>
-            <SignUpButton mode="modal" forceRedirectUrl="/t">
-              <Button>Sign up</Button>
-            </SignUpButton>
-          </div>
-        </SignedOut>
-      </ClerkProvider>
+      <ClerkLoading>
+        <div className="w-40 h-9" />
+      </ClerkLoading>
+      <SignedIn>
+        <OpenDashboardLinkButton />
+      </SignedIn>
+      <SignedOut>
+        <div className="flex gap-4 animate-[fade-in_0.2s]">
+          <SignInButton mode="modal" forceRedirectUrl="/t">
+            <Button variant="ghost">Sign in</Button>
+          </SignInButton>
+          <SignUpButton mode="modal" forceRedirectUrl="/t">
+            <Button>Sign up</Button>
+          </SignUpButton>
+        </div>
+      </SignedOut>
     </ErrorBoundary>
   );
 }
